ec9eff8aebf067142d7796b966d25da0c5e55971
wangchongwei <2311953425@qq.com>
2019-05-23 10:24:34 +0800

    Initial Commit

---
+++ myCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  myCode
+//
+//  Created by 王崇伟 on 2019/5/23.
+//  Copyright © 2019 wangchongwei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ myCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  myCode
+//
+//  Created by 王崇伟 on 2019/5/23.
+//  Copyright © 2019 wangchongwei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
